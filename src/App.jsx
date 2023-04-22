3df6186b464f3b0aed4f011efe92c325d742dc5c
snezhka003 <you@example.com>
2023-04-22 17:29:29 +0300

    Задача № 1

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import uniqid from "uniqid";
+import Clock from "./Components/Clock";
+import Form from "./Components/Form";
+import './App.css'
+
+export default function App() {
+  const [clocks, setClocks] = useState([]);
+
+  function handleFormSubmit(form) {
+    setClocks((prevState) => [...prevState, {
+      id: uniqid(),
+      name: form.name,
+      userTimezone: form.userTimezone,
+    }]);
+  }
+
+  function getClockIndex(id) {
+    const index = clocks.findIndex((clock) => clock.id === id);
+
+    return index;
+  }
+
+  function handleDeleteClick(id) {
+    const index = getClockIndex(id);
+
+    const updatedClocks = [
+      ...clocks.slice(0, index),
+      ...clocks.slice(index + 1),
+    ];
+
+    setClocks(updatedClocks);
+  }
+
+  return (
+    <div className="App-container">
+      <Form onFormSubmit={handleFormSubmit} />
+      <div className="App-clocks-container">
+        {clocks.map((clock) => {
+          return (
+            <Clock
+              key={clock.id}
+              id={clock.id}
+              name={clock.name}
+              userTimezone={clock.userTimezone}
+              onDeleteClick={handleDeleteClick}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
